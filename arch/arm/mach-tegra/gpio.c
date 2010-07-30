004b2c2cf6a5abd5319941a92bf7b1f7fcff3873
Gary King <gking@nvidia.com>
2010-07-29 18:22:30 -0700

    [ARM] tegra: introduce TEGRA_NR_GPIOS
    Tegra GPIO implementation defined ARCH_NR_GPIOS to be equal to number of
    GPIOs available on the Tegra SoC thus preventing usage of GPIO
    extensions. Introduction of TEGRA_NR_GPIOS allows gpiolib to
    manage non-Tegra GPIOs
    
    Original author: Mike Rapoport <mike@compulab.co.il>
    Signed-off-by: Mike Rapoport <mike@compulab.co.il>
    Signed-off-by: Gary King <gking@nvidia.com>
    Change-Id: Ia2e910f9da6fbef751b0463fe8a73e5d928d18f0

--- arch/arm/mach-tegra/gpio.c
+++ arch/arm/mach-tegra/gpio.c
@@ -144,7 +144,7 @@ static struct gpio_chip tegra_gpio_chip = {
 	.direction_output	= tegra_gpio_direction_output,
 	.set			= tegra_gpio_set,
 	.base			= 0,
-	.ngpio			= ARCH_NR_GPIOS,
+	.ngpio			= TEGRA_NR_GPIOS,
 };
 
 static void tegra_gpio_irq_ack(unsigned int irq)
@@ -281,7 +281,7 @@ void tegra_gpio_resume(void)
 
 	local_irq_restore(flags);
 
-	for (i=INT_GPIO_BASE; i<(INT_GPIO_BASE+ARCH_NR_GPIOS); i++) {
+	for (i=INT_GPIO_BASE; i<(INT_GPIO_BASE+TEGRA_NR_GPIOS); i++) {
 		struct irq_desc *desc = irq_to_desc(i);
 		if (!desc || (desc->status & IRQ_WAKEUP)) continue;
 		enable_irq(i);
@@ -294,7 +294,7 @@ void tegra_gpio_suspend(void)
 	int b, p, i;
 
 
-	for (i=INT_GPIO_BASE; i<(INT_GPIO_BASE+ARCH_NR_GPIOS); i++) {
+	for (i=INT_GPIO_BASE; i<(INT_GPIO_BASE+TEGRA_NR_GPIOS); i++) {
 		struct irq_desc *desc = irq_to_desc(i);
 		if (!desc) continue;
 		if (desc->status & IRQ_WAKEUP) {
@@ -361,7 +361,7 @@ static int __init tegra_gpio_init(void)
 
 	gpiochip_add(&tegra_gpio_chip);
 
-	for (i = INT_GPIO_BASE; i < (INT_GPIO_BASE + ARCH_NR_GPIOS); i++) {
+	for (i = INT_GPIO_BASE; i < (INT_GPIO_BASE + TEGRA_NR_GPIOS); i++) {
 		bank = &tegra_gpio_banks[GPIO_BANK(irq_to_gpio(i))];
 
 		lockdep_set_class(&irq_desc[i].lock, &gpio_lock_class);
