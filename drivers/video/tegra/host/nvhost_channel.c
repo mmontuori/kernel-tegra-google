415bc6dc385c84dd619b4a8767f027c8da46b7b9
Varun Wadekar <vwadekar@nvidia.com>
2010-11-23 19:43:43 +0530

    tegra: video: host: remove cancelled actions from the list at power down
    Change-Id: I2a82ecadafacec53990db25cb456809dbffd999b
    Author: Alex Frid <afrid@nvidia.com>
    Signed-off-by: Varun Wadekar <vwadekar@nvidia.com>

--- drivers/video/tegra/host/nvhost_channel.c
+++ drivers/video/tegra/host/nvhost_channel.c
@@ -214,6 +214,7 @@ static void power_3d(struct nvhost_module *mod, enum nvhost_power_action action)
 			struct nvhost_op_pair save;
 			struct nvhost_cpuinterrupt ctxsw;
 			u32 syncval;
+			void *ref;
 			syncval = nvhost_syncpt_incr_max(&ch->dev->syncpt,
 							NVSYNCPT_3D,
 							ch->cur_ctx->save_incrs);
@@ -232,7 +233,7 @@ static void power_3d(struct nvhost_module *mod, enum nvhost_power_action action)
 			nvhost_intr_add_action(&ch->dev->intr, NVSYNCPT_3D,
 					       syncval,
 					       NVHOST_INTR_ACTION_WAKEUP,
-					       &wq, NULL);
+					       &wq, &ref);
 			wait_event(wq,
 				   nvhost_syncpt_min_cmp(&ch->dev->syncpt,
 							 NVSYNCPT_3D, syncval));
