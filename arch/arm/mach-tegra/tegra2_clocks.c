5e41b49df5617e87a5ca3da78e1ec607902cd323
Varun Wadekar <vwadekar@nvidia.com>
2011-04-28 10:23:41 +0530

    ARM: tegra: add bsea to clk_duplicates
    Change-Id: I9b5d11756e0dfd5e5a3a21e1a463cd466a960b68
    Signed-off-by: Varun Wadekar <vwadekar@nvidia.com>

--- arch/arm/mach-tegra/tegra2_clocks.c
+++ arch/arm/mach-tegra/tegra2_clocks.c
@@ -2067,6 +2067,7 @@ struct clk_duplicate tegra_clk_duplicates[] = {
 	CLK_DUPLICATE("mpe", "tegra_grhost", "mpe"),
 	CLK_DUPLICATE("cop", "tegra-avp", "cop"),
 	CLK_DUPLICATE("vde", "tegra-aes", "vde"),
+	CLK_DUPLICATE("bsea", "tegra-aes", "bsea"),
 };
 
 #define CLK(dev, con, ck)	\
